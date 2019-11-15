c41b6e6df6523fbf49973d36952f2acad92942ba
Dipesh Singhal <dipesh.singhal@gmail.com>
2019-11-15 22:07:21 +0530

    Binary Search with Recursive and Non recursive way

--- Algorithm/src/com/study/code/BinarySearch.java
+++ Algorithm/src/com/study/code/BinarySearch.java
@@ -2,13 +2,13 @@ package com.study.code;
 
 public class BinarySearch {
 
-    //sort in ascending order using bubble sort
+    //recursive approach
     protected static int search(int[] array, int start, int end, int number){
 
         if ( start > end )
             return -1;
 
-        int mid = (start + end);
+        int mid = (start + end) / 2;
 
         if( number == array[mid]) {
             return mid;
@@ -21,6 +21,28 @@ public class BinarySearch {
 
     }
 
+    //Non-recursive approach
+    protected static int searchNonRecursive(int[] array, int number){
+
+        int start = 0;
+        int end = array.length - 1;
+
+        while( start <= end ) {
+            int mid = (start + end) / 2;
+            if (number == array[mid]) {
+                return mid;
+            } else if (number < array[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return -1;
+
+
+    }
+
     public static void main(String args[]) {
 
         int[] array = {0, 1, 2, 4, 5, 8, 12, 15, 20};
@@ -28,6 +50,12 @@ public class BinarySearch {
         System.out.println(" 5 found at: " + search(array, 0, array.length-1, 5));
 
         System.out.println(" 6 found at: " + search(array, 0, array.length-1, 6));
+
+        System.out.println("using searchNonRecursive");
+
+        System.out.println(" 5 found at (): " + searchNonRecursive(array, 5));
+
+        System.out.println(" 6 found at: " + searchNonRecursive(array, 6));
 
     }
 }
